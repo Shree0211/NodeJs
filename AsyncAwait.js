86e0974c706386e9a19798e567aa45a5ed08a4c8
shree0211 <shreevyas0211@gmail.com>
2022-04-18 23:56:35 +0530

    Use of Await keyword

--- AsyncAwait.js
+++ AsyncAwait.js
@@ -1,9 +1,35 @@
-//Simple Async/Await example
-const one = async() => {
-    var y = await "Hello World";
-    console.log(y);
-}
+// //Simple Async/Await example
+// const one = async() => {
+//     var y = await "Hello World";
+//     console.log(y);
+// }
   
-console.log(1);
-one();
-console.log(2);
+// console.log(1);
+// one();
+// console.log(2);
+
+//use of await keyword 
+let is_shop_open = true;
+
+let modification = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(console.log("Modification held"));
+        },3000);
+    });
+};
+
+async function order()
+{
+    console.log("Order received");
+    console.log("Production started");
+    console.log("Item preparing");
+
+    await modification();
+    console.log("Order completed");
+    console.log("Waiting for new order");
+}
+
+order();
+
+console.log("-------NEW OFFERS--------");
