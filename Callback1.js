20585fea413dd36887f646efdf8e7b560c3419d9
shree0211 <shreevyas0211@gmail.com>
2022-04-18 21:59:49 +0530

    SetTimer function with callback

--- Callback1.js
+++ Callback1.js
@@ -12,17 +12,45 @@
 
 //Addition of 2 numbers
 
-function Sum(num1,num2,add)
-{
-    let addition = num1+num2;
-    console.log("Addition of 2 numbers is :", addition);
-    add(10,20);
-}
+// function Sum(num1,num2,add)
+// {
+//     let addition = num1+num2;
+//     console.log("Addition of 2 numbers is :", addition);
+//     add(10,20);
+// }
 
-const Sum1 = (num1,num2) =>
-{
-    console.log("Addition of Sum1 is :", num1+num2);
-}
-Sum(30,40,Sum1);
+// const Sum1 = (num1,num2) =>
+// {
+//     console.log("Addition of Sum1 is :", num1+num2);
+// }
+// Sum(30,40,Sum1);
 
-console.log();
+// console.log();
+
+
+//Using set timer function
+
+let order =(call_production) =>
+{
+    setTimeout(()=>{
+        console.log("Order placed, please call production...");
+        call_production();
+    },2000)
+    
+}
+let production=() => 
+{
+    setTimeout(()=>{
+        console.log("Production started...");
+
+        setTimeout(()=>{
+            console.log("Item preparing...");
+
+            setTimeout(()=>{
+                console.log("Production completed.");
+            
+            },2000);  
+        },3000);        
+    },5000);
+}
+order(production);
