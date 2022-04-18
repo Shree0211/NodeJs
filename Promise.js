ed7899f2fb8aad18757a32cda04388808d0e3253
shree0211 <shreevyas0211@gmail.com>
2022-04-18 23:21:54 +0530

    Promise using set timer

--- Promise.js
+++ Promise.js
@@ -1,19 +1,49 @@
 //Simple Promise example
-var promise = new Promise(function(resolve, reject) {
-    const x = "HelloWorld";
-    const y = "HelloWorld"
-    if(x === y) {
-      resolve();
-    } else {
-      reject();
-    }
-  });
+// var promise = new Promise(function(resolve, reject) {
+//     const x = "HelloWorld";
+//     const y = "HelloWorld"
+//     if(x === y) {
+//       resolve();
+//     } else {
+//       reject();
+//     }
+//   });
      
-  promise
+//   promise
   
-      .then(function () {
-          console.log('Successfull');
-      })
-      .catch(function () {
-          console.log('Some error has occurred');
-      });
+//       .then(function () {
+//           console.log('Successfull');
+//       })
+//       .catch(function () {
+//           console.log('Some error has occurred');
+//       });
+
+let is_shop_open = true;
+function order(time, work) {
+    return new Promise((resolve, reject) => {
+
+        if (is_shop_open) {
+
+            setTimeout(() => {
+                resolve(work())
+            }, time);
+        }
+
+        else {
+            reject(console.log("Shop is called"))
+        }
+    });
+};
+order(2000,()=>console.log("Order received") )
+
+.then(() => {
+    return order(1000, () => console.log("Production has started..."))
+} )
+
+.then(() => {
+    return order(1000, () => console.log("Item preparing..."))
+} )
+
+.then(() => {
+    return order(1000, () => console.log("Production completed."))
+} );
