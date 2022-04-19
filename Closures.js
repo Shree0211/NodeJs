71d0a24a4facb5306082af67579db494580644da
shree0211 <shreevyas0211@gmail.com>
2022-04-19 16:52:19 +0530

    Calling after multiple lines of codes

--- Closures.js
+++ Closures.js
@@ -1,4 +1,4 @@
-//Function along with the lexical scope forms closoure
+// //Function along with the lexical scope forms closoure
 function x() {
     var a= 10;  //lexical scope
     function y() {
@@ -7,3 +7,16 @@ function x() {
     y();
 }
 x();
+
+//Calling after multiple lines of code
+function x(){
+    var b = 20;
+    function y(){
+        console.log(b);
+    }
+    return y;
+}
+var z = x();
+console.log(z);
+//......
+z();
