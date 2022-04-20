eb34119f82683dea0437c35c02a3b0f70e9d268a
shree0211 <shreevyas0211@gmail.com>
2022-04-20 18:30:14 +0530

    Implementation of spread operator

--- es6.js
+++ es6.js
@@ -89,3 +89,11 @@ function address(city,state){
     console.log(newaddress);
 }
 address('rajkot','gujarat');
+
+//spread operator
+
+let example1=[1,2,3,4,5]
+let example2=[...example1]
+example2.push(true)
+console.log(example2);
+
